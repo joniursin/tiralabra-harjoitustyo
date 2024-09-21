aee2f4a31edc68f86ec3d746dd0b9f7ee4c47ce4
joni ursin <joni.ursin@gmail.com>
2024-09-21 22:59:17 +0300

    added mnist data to directory data, updated main.py adding training and testing to the network, finished docstring

--- src/main.py
+++ src/main.py
@@ -1,20 +1,16 @@
 import numpy as np
+import idx2numpy
 
 class Network:
-    """Luokka, joka luo neuroverkon ja kouluttaa sitä.
-
-    Attributes:
-        input_layer: Neuroverkon 1 tason koko.
-        hidden_layer: Neuroverkon 2 tason koko.
-        output_layer: Neuroverkon 3 tason koko.
+    """Class, which creates a neural network and trains it.
     """
-    def __init__(self, input_layer=784, hidden_layer=100, output_layer=10):
-        """Luokan konstruktori, joka luo neuroverkon
+    def __init__(self, input_layer, hidden_layer, output_layer):
+        """Contructor, which initializes the network and its weights and biases.
 
         Args:
-            input_layer (int, optional): _description_. Defaults to 784.
-            hidden_layer (int, optional): _description_. Defaults to 100.
-            output_layer (int, optional): _description_. Defaults to 10.
+            input_layer (int): Number of neurons in the input layer.
+            hidden_layer (int): Number of neurons in the hidden layer.
+            output_layer (int): Number of neurons in the output layer.
         """
         self.input_layer = input_layer
         self.hidden_layer = hidden_layer
@@ -22,80 +18,102 @@ class Network:
         self.biases = []
         self.weights = []
 
-        self.biases.append(np.zeros((1, self.hidden_layer)))
-        self.weights.append(np.random.randn(self.input_layer, self.hidden_layer))
+        self.biases.append(np.zeros([self.hidden_layer, 1]))
+        self.weights.append(np.random.randn(self.hidden_layer, self.input_layer) * np.sqrt(2/self.input_layer))
 
-        self.biases.append(np.zeros((1, self.output_layer)))
-        self.weights.append(np.random.randn(self.hidden_layer, self.output_layer))
+        self.biases.append(np.zeros([self.output_layer, 1]))
+        self.weights.append(np.random.randn(self.output_layer, self.hidden_layer))
 
-    def feedforward(self, input):
-        """_summary_
+    def feedforward(self, x):
+        """Forward propagates through the network.
 
         Args:
-            input (_type_): _description_
-
+            x (np.array): Input data.
         Returns:
-            _type_: _description_
+            np.array: Predicted output.
         """
-        self.hidden_activation = np.dot(input, self.weights[0]) + self.biases[0]
+        self.hidden_activation = np.dot(self.weights[0], x) + self.biases[0]
         self.hidden_output = sigmoid(self.hidden_activation)
 
-        self.output_activation = np.dot(self.hidden_output, self.weights[1]) + self.biases[1]
+        self.output_activation = np.dot(self.weights[1], self.hidden_output) + self.biases[1]
         self.predicted_output = sigmoid(self.output_activation)
 
         return self.predicted_output
     
     def backpropagation(self, x, y, rate):
-        """_summary_
+        """Backpropagation algorithm, which updates the weights and biases.
 
         Args:
-            x (_type_): _description_
-            y (_type_): _description_
-            rate (_type_): _description_
+            x (np.array): Input data.
+            y (np.array): Value of the input data.
+            rate (float): Learning rate.
         """
         output_error = y - self.predicted_output
         output_delta = output_error * sigmoid_derivative(self.predicted_output)
 
-        hidden_error = np.dot(output_delta, self.weights[1].T)
-        hidden_delta = hidden_error * sigmoid_derivative(self.hidden_output)
+        hidden_error = output_delta.T.dot(self.weights[1])
+        hidden_delta = hidden_error.T * sigmoid_derivative(self.hidden_output)
 
-        self.weights[1] += np.dot(self.hidden_output.T, output_delta) * rate
+        self.weights[1] += self.hidden_output.dot(output_delta.T).T * rate
         self.biases[1] += np.sum(output_delta, axis=0, keepdims=True) * rate
-        self.weights[0] += np.dot(x.T, hidden_delta) * rate
+        self.weights[0] += x.dot(hidden_delta.T).T * rate
         self.biases[0] += np.sum(hidden_delta, axis=0, keepdims=True) * rate
     
-    def train(self, x, y, epochs, rate):
-        """_summary_
+    def train(self, x, y, rate):
+        """Trains the network using input data and it's values.
 
         Args:
-            x (_type_): _description_
-            y (_type_): _description_
-            epochs (_type_): _description_
-            rate (_type_): _description_
+            x (np.array): Input data.
+            y (np.array): Value of the input data.
+            rate (_float): Learning rate.
         """
-        for i in range(epochs):
-            output = self.feedforward(x)
-            self.backpropagation(x, y, rate)
-            loss = np.mean(np.square(y - output))
+        self.feedforward(x)
+        self.backpropagation(x, y, rate)
 
 def sigmoid(x):
-    """_summary_
+    """Calculates sigmoid.
 
     Args:
-        x (_type_): _description_
+        x (np.array): Value that gets calculated.
 
     Returns:
-        _type_: _description_
+        np.array: Sigmoid.
     """
+    print(x)
     return 1/(1 + np.exp(-x))
 
 def sigmoid_derivative(x):
-    """_summary_
+    """Calculates sigmoid derivative.
 
     Args:
-        x (_type_): _description_
+        x (np.array): Value that gets calculated.
 
     Returns:
-        _type_: _description_
+        np.array: Sigmoid derivative.
     """
     return x * (1-x)
+
+# Move these to functions/files
+X_train = idx2numpy.convert_from_file('data/train-images-idx3-ubyte')/255
+X_train = X_train.reshape(X_train.shape[0], -1)
+y = idx2numpy.convert_from_file('data/train-labels-idx1-ubyte')
+y_train = np.zeros((y.size, 10))
+y_train[np.arange(y.size), y] = 1
+
+X_test = idx2numpy.convert_from_file('data/t10k-images-idx3-ubyte')/255
+X_test = X_test.reshape(X_test.shape[0], -1)
+y = idx2numpy.convert_from_file('data/t10k-labels-idx1-ubyte')
+y_test = np.zeros((y.size, 10))
+y_test[np.arange(y.size), y] = 1
+
+nn = Network(784, 100, 10)
+
+for i in range(X_train.shape[0]):
+    x = np.array(X_train[i, :].reshape(-1,1))
+    y = np.array(y_train[i, :].reshape(-1,1))
+    nn.train(x, y, 0.1)
+
+for i in range(X_test.shape[0]): 
+    x = np.array(X_test[i].reshape(-1,1))
+    predict = nn.feedforward(x)
+    print("was:",np.argmax(y_test[i]), "predict:",np.argmax(predict), predict[np.argmax(predict)])
