f5a230ff73c9482d89f23679c7f436c771e0abe6
joni ursin <joni.ursin@gmail.com>
2024-09-28 21:22:41 +0300

    renamed main.py to network.py added train_network.py

--- src/network.py
+++ src/network.py
@@ -85,7 +85,6 @@ def sigmoid(x):
     Returns:
         np.array: Sigmoid.
     """
-    print(x)
     return 1/(1 + np.exp(-x))
 
 
@@ -99,30 +98,3 @@ def sigmoid_derivative(x):
         np.array: Sigmoid derivative.
     """
     return x * (1-x)
-
-
-# Move these to functions/files
-X_train = idx2numpy.convert_from_file('data/train-images-idx3-ubyte')/255
-X_train = X_train.reshape(X_train.shape[0], -1)
-y = idx2numpy.convert_from_file('data/train-labels-idx1-ubyte')
-y_train = np.zeros((y.size, 10))
-y_train[np.arange(y.size), y] = 1
-
-X_test = idx2numpy.convert_from_file('data/t10k-images-idx3-ubyte')/255
-X_test = X_test.reshape(X_test.shape[0], -1)
-y = idx2numpy.convert_from_file('data/t10k-labels-idx1-ubyte')
-y_test = np.zeros((y.size, 10))
-y_test[np.arange(y.size), y] = 1
-
-nn = Network(784, 100, 10)
-
-for i in range(X_train.shape[0]):
-    x = np.array(X_train[i, :].reshape(-1, 1))
-    y = np.array(y_train[i, :].reshape(-1, 1))
-    nn.train(x, y, 0.1)
-
-for i in range(X_test.shape[0]):
-    x = np.array(X_test[i].reshape(-1, 1))
-    predict = nn.feedforward(x)
-    print("was:", np.argmax(y_test[i]), "predict:", np.argmax(
-        predict), predict[np.argmax(predict)])
