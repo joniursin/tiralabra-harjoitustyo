693730c35a3ebcc62102eec4aa431c3b582711b9
joni ursin <joni.ursin@gmail.com>
2024-10-05 20:25:45 +0300

    added canvas.py

---
+++ src/canvas.py
@@ -0,0 +1,44 @@
+import tkinter as tk
+from PIL import Image
+import numpy as np
+import train_network
+import os
+
+class Canvas:
+    def __init__(self, app):
+        self.app = app
+        self.app.title("Draw")
+        self.canvas = tk.Canvas(app, width=280, height=280, bg="white")
+        self.canvas.pack()
+        self.canvas.bind("<B1-Motion>", self.draw)
+
+        self.clear_button = tk.Button(app, text="clear", command=self.clear)
+        self.clear_button.pack(side=tk.RIGHT)
+
+        self.image = Image.new("L", (28, 28), color=0)
+
+    def draw(self, event):
+        x, y = event.x, event.y
+        if 0 <= x < 280 and 0 <= y < 280:
+            self.canvas.create_oval(x, y, x + 10, y + 10, fill="black", width=2)
+            self.image.putpixel((x  // 10, y // 10), 255)
+
+            self.predict_image()
+
+    def clear(self):
+        self.canvas.delete("all")
+        self.image = Image.new("L", (28, 28), color=0)
+
+    def predict_image(self):
+        data = np.array(self.image)/255
+        data = np.array(data.reshape(-1, 1))
+        predict = train_network.test_network(data)
+        predict_number = np.argmax(predict)
+        predict_percentage = predict[np.argmax(predict)]
+        
+        print(f"Predicted '{predict_number}' {float(predict_percentage)*100:,.2f}%")
+
+os.system("train_network.py")
+app = tk.Tk()
+app_app = Canvas(app)
+app.mainloop()
--- src/train_network.py
+++ src/train_network.py
@@ -2,6 +2,9 @@ import numpy as np
 import idx2numpy
 from network import Network
 
+def test_network(test_data):
+    return nn.feedforward(test_data)
+
 X_train = idx2numpy.convert_from_file('data/train-images-idx3-ubyte')/255
 X_train = X_train.reshape(X_train.shape[0], -1)
 y = idx2numpy.convert_from_file('data/train-labels-idx1-ubyte')
@@ -16,13 +19,13 @@ y_test[np.arange(y.size), y] = 1
 
 nn = Network(784, 100, 10)
 
-print("Training network...")
+print("Training network... (should take 1-2 minutes)")
 for i in range(X_train.shape[0]):
     x = np.array(X_train[i, :].reshape(-1, 1))
     y = np.array(y_train[i, :].reshape(-1, 1))
     nn.train(x, y, 0.1)
 
-input("Training done, start testing [enter]")
+print("Training done, testing accuracy...")
 CORRECT = 0
 for i in range(X_test.shape[0]):
     x = np.array(X_test[i].reshape(-1, 1))
