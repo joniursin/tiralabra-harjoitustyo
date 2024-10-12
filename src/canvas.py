341b4fe34e12a9103c65ac3ccc990f71fd0eaef0
joni ursin <joni.ursin@gmail.com>
2024-10-12 21:26:52 +0300

    modified canvas.py

--- src/canvas.py
+++ src/canvas.py
@@ -1,9 +1,7 @@
 import tkinter as tk
-from PIL import Image
+from PIL import Image, ImageDraw
 import numpy as np
 import train_network
-import os
-
 
 class Canvas:
     def __init__(self, app):
@@ -16,33 +14,36 @@ class Canvas:
         self.clear_button = tk.Button(app, text="clear", command=self.clear)
         self.clear_button.pack(side=tk.RIGHT)
 
-        self.image = Image.new("L", (28, 28), color=0)
+        self.predict_button = tk.Button(app, text="predict", command=self.predict_image)
+        self.predict_button.pack(side=tk.LEFT)
+
+        self.predict_label = tk.Label(app, text="")
+        self.predict_label.pack()
+
+        self.image = Image.new("L", (280, 280), color=0)
+        self.image_draw = ImageDraw.Draw(self.image)
 
     def draw(self, event):
         x, y = event.x, event.y
-        if 0 <= x < 280 and 0 <= y < 280:
-            self.canvas.create_oval(
-                x, y, x + 10, y + 10, fill="black", width=2)
-            self.image.putpixel((x // 10, y // 10), 255)
-
-            self.predict_image()
+        self.canvas.create_oval(
+            x - 10, y - 10, x + 10, y + 10, fill="black", width=10)
+        self.image_draw.ellipse([x - 10, y - 10, x + 10, y + 10], fill="white")
 
     def clear(self):
         self.canvas.delete("all")
-        self.image = Image.new("L", (28, 28), color=0)
+        self.image = Image.new("L", (280, 280), color=0)
+        self.image_draw = ImageDraw.Draw(self.image)
+        self.predict_label.config(text="")
 
     def predict_image(self):
-        data = np.array(self.image)/255
+        resize_image = self.image.resize((28, 28))
+        data = np.array(resize_image)/255.0
         data = np.array(data.reshape(-1, 1))
         predict = train_network.test_network(data)
         predict_number = np.argmax(predict)
         predict_percentage = predict[np.argmax(predict)]
+        self.predict_label.config(text=f"Predicted '{predict_number}' ({float(predict_percentage)*100:,.2f}%)")
 
-        print(
-            f"Predicted '{predict_number}' {float(predict_percentage)*100:,.2f}%")
-
-
-os.system("train_network.py")
 app = tk.Tk()
 app_app = Canvas(app)
 app.mainloop()
