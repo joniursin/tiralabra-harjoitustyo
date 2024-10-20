d33589a66a67cd8c7630dded3dd69799611bf4b7
joni ursin <joni.ursin@gmail.com>
2024-10-20 19:12:45 +0300

    fixed number drawing for better predictions added docstring

--- src/canvas.py
+++ src/canvas.py
@@ -1,10 +1,15 @@
 import tkinter as tk
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageOps
 import numpy as np
 import train_network
 
 class Canvas:
     def __init__(self, app):
+        """Creates canvas for drawing numbers
+
+        Args:
+            app (Tk): Tkinter gui window.
+        """
         self.app = app
         self.app.title("Draw")
         self.canvas = tk.Canvas(app, width=280, height=280, bg="white")
@@ -20,23 +25,33 @@ class Canvas:
         self.predict_label = tk.Label(app, text="")
         self.predict_label.pack()
 
-        self.image = Image.new("L", (280, 280), color=0)
+        self.image = Image.new("L", (280, 280), color=255)
         self.image_draw = ImageDraw.Draw(self.image)
 
     def draw(self, event):
+        """Adds oval to the given canvas and ellipse to PIL image.
+
+        Args:
+            event (tk.Canvas): Canvas that is being updated.
+        """
         x, y = event.x, event.y
         self.canvas.create_oval(
-            x - 10, y - 10, x + 10, y + 10, fill="black", width=10)
-        self.image_draw.ellipse([x - 10, y - 10, x + 10, y + 10], fill="white")
+            x - 5, y - 5, x + 5, y + 5, fill="black", width=10)
+        self.image_draw.ellipse([x - 5, y - 5, x + 5, y + 5], fill="black")
+        self.predict_image()
 
     def clear(self):
+        """Clears the canvas and PIL image.
+        """
         self.canvas.delete("all")
-        self.image = Image.new("L", (280, 280), color=0)
+        self.image = Image.new("L", (280, 280), color=255)
         self.image_draw = ImageDraw.Draw(self.image)
         self.predict_label.config(text="")
 
     def predict_image(self):
-        resize_image = self.image.resize((28, 28))
+        """Resizes the PIL image, turns it into np.array and sends it to the neural network for prediction.
+        """
+        resize_image = ImageOps.invert(self.image.resize((28, 28)))
         data = np.array(resize_image)/255.0
         data = np.array(data.reshape(-1, 1))
         predict = train_network.test_network(data)
